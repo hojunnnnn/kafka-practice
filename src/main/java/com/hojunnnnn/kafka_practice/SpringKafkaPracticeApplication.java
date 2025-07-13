d584c3eb6f540611831decf0cfba63037d22acd6
HJLee <gotjd8515@gmail.com>
2025-07-13 18:29:21 +0900

    Initial commit

---
+++ src/main/java/com/hojunnnnn/kafka_practice/SpringKafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hojunnnnn.kafka_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hojunnnnn/kafka_practice/SpringKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojunnnnn.kafka_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
