75d0f99d42a2c1be238b6868cfe6766036bab1bf
HJLee <gotjd8515@gmail.com>
2025-07-14 15:00:01 +0900

    create OrderEventOutbox entity

---
+++ src/main/java/com/hojunnnnn/kafka_practice/order/domain/OrderEventOutbox.java
@@ -0,0 +1,31 @@
+package com.hojunnnnn.kafka_practice.order.domain;
+
+import com.hojunnnnn.kafka_practice.common.domain.BaseTimeEntity;
+import com.hojunnnnn.kafka_practice.order.domain.type.EventStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderEventOutbox extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "event_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EventStatus eventStatus;
+
+    public OrderEventOutbox(Long orderId) {
+        this.orderId = orderId;
+        this.eventStatus = EventStatus.INIT;
+    }
+}
---
+++ src/main/java/com/hojunnnnn/kafka_practice/order/domain/type/EventStatus.java
@@ -0,0 +1,5 @@
+package com.hojunnnnn.kafka_practice.order.domain.type;
+
+public enum EventStatus {
+    INIT, PUBLISHED, FAILED;
+}
