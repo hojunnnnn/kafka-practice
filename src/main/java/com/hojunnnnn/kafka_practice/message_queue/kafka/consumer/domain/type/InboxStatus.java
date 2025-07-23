4955e3dff36e180afdaea7d0a3c8038a4a752dd2
HJLee <gotjd8515@gmail.com>
2025-07-23 16:14:20 +0900

    move package to domain

--- src/main/java/com/hojunnnnn/kafka_practice/message_queue/kafka/consumer/domain/type/InboxStatus.java
+++
@@ -1,7 +0,0 @@
-package com.hojunnnnn.kafka_practice.message_queue.kafka.consumer.domain.type;
-
-public enum InboxStatus {
-    PROCESSING,
-    COMPLETED,
-    FAILED
-}
---
+++ src/main/java/com/hojunnnnn/kafka_practice/order/domain/type/InboxStatus.java
@@ -0,0 +1,7 @@
+package com.hojunnnnn.kafka_practice.order.domain.type;
+
+public enum InboxStatus {
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
